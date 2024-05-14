41cd5228e8bf52b8bb0d72948fbc1c2d45ede966
Kason <kason.suchow@gmail.com>
2024-05-13 18:13:50 -0600

    Animation system is functional!
    Default animation runs at 2 frames per second, this will probably be changed to require an actual frame input. Animation speed can be overwritten.
    
    Animation can also be mirrored (this works great for the stick figure testing character but will probably not be useful for the actual player character if the character is not symetrical.

--- animation.py
+++ animation.py
@@ -1,17 +1,22 @@
+import pygame
+
 import lib
 
 class Animation():
-    def __init__(self, frames: list) -> None:
+    def __init__(self, frames: list, animation_speed = 2) -> None:
         self.frames = frames
         self.frame_index = 0
-        self.animate_speed = 2
+        self.animation_speed = animation_speed
 
-    def animate(self):
-        self.frame_index += self.animate_speed * lib.delta_time
+    def animate(self, mirrored = False):
+        self.frame_index += self.animation_speed * lib.delta_time
 
         if self.frame_index > len(self.frames):
             self.frame_index = 0
 
-        image = self.frames[int(self.frame_index)]
+        if mirrored:
+            image = pygame.transform.flip(self.frames[int(self.frame_index)], True, False)
+        else:
+            image = self.frames[int(self.frame_index)]
 
         return image
--- player.py
+++ player.py
@@ -21,7 +21,14 @@ class Player(pygame.sprite.Sprite):
         self.idle_animation = animation.Animation([
             pygame.image.load("assets/animations/player/idle/idle_1.png").convert_alpha(),
             pygame.image.load("assets/animations/player/idle/idle_2.png").convert_alpha()
-            ])
+        ])
+        
+        self.walking_animation = animation.Animation([
+            pygame.image.load("assets/animations/player/walking/walking_1.png").convert_alpha(),
+            pygame.image.load("assets/animations/player/walking/walking_2.png").convert_alpha(),
+            pygame.image.load("assets/animations/player/walking/walking_3.png").convert_alpha(),
+            pygame.image.load("assets/animations/player/walking/walking_4.png").convert_alpha()
+        ], animation_speed=8)
 
     def update(self) -> None:
         self.pos += self.velo * lib.delta_time
@@ -34,8 +41,10 @@ class Player(pygame.sprite.Sprite):
 
         if keys[pygame.K_a]:
             self.velo.x = -self.speed
+            self.image = self.walking_animation.animate(mirrored=True)
         elif keys[pygame.K_d]:
             self.velo.x = self.speed
+            self.image = self.walking_animation.animate()
         else:
             self.velo.x = 0
             self.image = self.idle_animation.animate()
